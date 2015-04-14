918cbb136514e1a1fbf3f6a7342178996be18a34
Christoph Burschka <christoph@burschka.de>
2015-04-14 17:21:56 +0200

    Issue #2470944: Convert settings forms to D8

---
+++ src/Form/XBBCodeHandlerForm.php
@@ -0,0 +1,168 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\xbbcode\Form\XBBCodeTagForm.
+ */
+
+namespace Drupal\xbbcode\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+class XBBCodeHandlerForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'xbbcode_handlers';
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $format = XBBCODE_GLOBAL) {
+    // Load the database interface.
+    module_load_include('inc', 'xbbcode', 'xbbcode.crud');
+    // Find out which formats use global settings.
+    $formats = xbbcode_formats();
+
+    $form = [];
+
+    if ($format == XBBCODE_GLOBAL) {
+      $form = self::_buildFormGlobal($form);
+    }
+
+    drupal_add_css(drupal_get_path('module', 'xbbcode') . '/xbbcode.css');
+    drupal_add_js(drupal_get_path('module', 'xbbcode') . '/xbbcode.js');
+    module_load_include('inc', 'xbbcode');
+    $handlers = _xbbcode_build_handlers();
+    $defaults = xbbcode_handlers_load($format, TRUE);
+
+    $form['tags'] = [
+      '#type' => 'fieldset',
+      '#theme' => 'xbbcode_settings_handlers_format',
+      '#tree' => TRUE,
+      '#title' => t('Tag settings'),
+      '#collapsible' => TRUE,
+      '#collapsed' => FALSE,
+    ];
+
+    $form['tags']['_enabled'] = [
+      '#type' => 'tableselect',
+      '#header' => [
+        'tag' => t('BBCode tag'),
+        'module' => t('Module'),
+      ],
+      '#default_value' => [],
+      '#options' => [],
+      '#attributes' => ['id' => 'xbbcode-handlers'],
+      '#empty' => t('No tags or handlers are defined. Please <a href="@modules">install a tag module</a> or <a href="@custom">create some custom tags</a>.', ['@modules' => url('admin/modules', ['fragment' => 'edit-modules-xbbcode']), '@custom' => url('admin/config/content/xbbcode/tags')]),
+    ];
+
+    foreach ($handlers as $name => $handler) {
+      $form['tags']['_enabled']['#options'][$name] = [
+        'tag' => [
+          'data' => _xbbcode_build_descriptions($name, $handler['info'], $defaults[$name]->module),
+          'class' => ['xbbcode-tag-description', 'xbbcode-tag-td'],
+        ],
+        'module' => [
+          'data' => 'handler-select',
+          'class' => ['xbbcode-tag-td'],
+        ],
+        '#attributes' => ['class' => $defaults[$name]->enabled ? ['selected'] : []],
+      ];
+      $form['tags']['_enabled']['#default_value'][$name] = $defaults[$name]->enabled ? 1 : NULL;
+
+      $form['tags'][$name]['module'] = [
+        '#type' => 'select',
+        '#options' => $handler['modules'],
+        '#default_value' => $defaults[$name]->module,
+        '#attributes' => ['class' => ['xbbcode-tag-handler']],
+      ];
+    }
+    return $form;
+
+  }
+
+  /**
+   * Modify the global handler settings.
+   */
+  private function _buildFormGlobal(array $form) {
+    $form['global'] = [
+      '#weight' => -1,
+      '#markup' => t('You are changing the global settings.'),
+    ];
+
+    foreach ($formats as &$list) {
+      foreach ($list as $format_id => $format_name) {
+        $list[$format_id] = l($format_name, 'admin/config/content/formats/' . $format_id);
+      }
+    }
+
+    if (!empty($formats['specific'])) {
+      if (!empty($formats['global'])) {
+        $form['global']['#markup'] .= ' ' . t('The following formats are affected by the global settings:');
+        $form['global']['#markup'] .= '<ul><li>' . implode('</li><li>', $formats['global']) . '</li></ul>';
+      }
+      else {
+        $form['global']['#markup'] .= ' ' . t('All formats using XBBCode currently override the global settings, so they have no effect.');
+      }
+      $form['global']['#markup'] .= ' ' . t('The following formats override the global settings, and will not be affected:');
+      $form['global']['#markup'] .= '<ul><li>' . implode('</li><li>', $formats['specific']) . '</li></ul>';
+    }
+    else {
+      $form['global']['#markup'] .= ' ' . t('All formats currently follow the global settings.');
+    }
+
+    $form['save'] = [
+      '#type' => 'submit',
+      '#value' => t('Save'),
+    ];
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  function submitForm(array &$form, FormStateInterface $form_state) {
+    // Determine if the settings are edited globally or in a text format.
+    if (isset($form['#format'])) {
+      // If a format has just been created, the #format info is still empty.
+      if (!empty($form['#format']->format)) {
+        $format_id = $form['#format']->format;
+      }
+      else {
+        $format_id = $form_state['values']['format'];
+      }
+      $settings = $form_state['values']['filters']['xbbcode']['settings'];
+    }
+    else {
+      $format_id = XBBCODE_GLOBAL;
+      $settings = $form_state['values'];
+    }
+
+    if ($format_id == XBBCODE_GLOBAL || $settings['override']) {
+      // Change the global settings or a format with specific settings.
+      $enabled = $settings['tags']['_enabled'];
+      unset($settings['tags']['_enabled']);
+      foreach ($settings['tags'] as $name => $values) {
+        if (is_array($values)) {
+          $values['name'] = $name;
+          $values['enabled'] = $enabled[$name] ? 1 : 0;
+          xbbcode_handler_save((object)$values, $format_id);
+        }
+      }
+      drupal_set_message(t('The tag settings were updated.'));
+      xbbcode_rebuild_tags($format_id);
+    }
+    else {
+      // If the format doesn't override, remove any specific settings.
+      if (xbbcode_handlers_delete_format($format_id)) {
+        drupal_set_message(t('The format-specific tag settings were reset.'));
+        xbbcode_rebuild_tags($format_id);
+      }
+    }
+  }
+}
---
+++ src/Form/XBBCodeTagForm.php
@@ -0,0 +1,276 @@
+<?php
+
+/**
+ * @file
+ * Contains \Drupal\xbbcode\Form\XBBCodeTagForm.
+ */
+
+namespace Drupal\xbbcode\Form;
+
+use Drupal\Core\Form\FormBase;
+use Drupal\Core\Form\FormStateInterface;
+
+/**
+ * List custom tags and edit or delete them.
+ */
+class XBBCodeTagForm extends FormBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getFormId() {
+    return 'xbbcode_tags';
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @param $name
+   *   If passed, load this tag for editing. Otherwise, list all tags and show a
+   *   collapsed tag creation form.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state, $name = NULL) {
+    module_load_include('inc', 'xbbcode', 'xbbcode.crud');
+    // Determine whether the user has loaded an existing tag for editing (via edit link).
+    $editing_tag = !empty($name);
+    // If the form was submitted, then a new tag is being added.
+    $adding_tag = !empty($form_state['input']) && $form_state['input']['op'] == t('Save');
+    $access_php = module_exists('php') && user_access('use PHP for settings');
+    $use_php = FALSE;
+
+    // The upshot is that if a tag is being edited or added, the otherwise optional fields become required.
+
+    // If editing a tag, load this tag and populate the form with its values.
+    if ($editing_tag) {
+      $tag = xbbcode_custom_tag_load($name);
+      $use_php = $tag->options['php'];
+      $form['edit'] = [
+        '#type' => 'fieldset',
+        '#title' => t('Editing Tag %name', ['%name' => $name]),
+        '#collapsible' => FALSE,
+      ];
+    }
+    else {
+      $tags = array_keys(xbbcode_custom_tag_load());
+
+      // If any tags already exist, build a list for deletion and editing.
+      if (!empty($tags)) {
+        foreach ($tags as $tag) {
+          $options[$tag] = '[' . $tag . '] ' . l(t('Edit'), "admin/config/content/xbbcode/tags/$tag/edit");
+        }
+        $form['existing'] = [
+          '#type' => 'checkboxes',
+          '#title' => t('Existing tags'),
+          '#description' => t('Check these tags and click "Delete" to delete them.'),
+          '#options' => $options,
+        ];
+      }
+      else {
+        // If no tags exist, then a new tag must be added now.
+        $adding_tag = TRUE;
+      }
+
+      $form['edit'] = [
+        '#type' => 'fieldset',
+        '#title' => t('Create new tag'),
+        '#collapsible' => TRUE,
+        '#collapsed' => count($tags),
+      ];
+
+      // Create an empty tag.
+      $tag = (object)[
+        'name' => '',
+        'description' => '',
+        'markup' => '',
+        'sample' => '',
+      ];
+    }
+
+    // Regardless of whether a new tag or an existing tag is being edited,
+    // show the edit form now. The fields are required only if a new tag is being
+    // saved (during the submission phase), or if an existing tag is being edited.
+
+    $form['edit']['name'] = [
+      '#type' => 'textfield',
+      '#default_value' => $tag->name,
+      '#field_prefix' => '[',
+      '#field_suffix' => ']',
+      '#required' => $editing_tag || $adding_tag,
+      '#maxlength' => 32,
+      '#size' => 16,
+      '#description' => t('The name of this tag. The name will be used in the text as [name]...[/name]. Must be alphanumeric and will automatically be converted to lowercase.'),
+    ];
+
+    $form['edit']['description'] = [
+      '#type' => 'textarea',
+      '#title' => t('Description'),
+      '#default_value' => $tag->description,
+      '#required' => $editing_tag || $adding_tag,
+      '#description' => t('This will be shown on help pages'),
+    ];
+
+    $form['edit']['sample'] = [
+      '#type' => 'textfield',
+      '#title' => t('Sample tag'),
+      '#required' => $editing_tag || $adding_tag,
+      '#description' => t('Enter an example of how this tag would be used. It will be shown on the help pages.'),
+      '#default_value' => $tag->sample,
+    ];
+
+    $form['edit']['options'] = [
+      '#type' => 'checkboxes',
+      '#title' => t('Tag options'),
+      '#options' => [
+        'selfclosing' => t('Tag is self-closing (requires no closing tag, like <code>[img]</code>).'),
+        'nocode' => t('Ignore further BBCode inside this tag.'),
+        'plain' => t('Escape all HTML inside this tag.'),
+      ],
+      '#description' => t('The last two options should in most cases be used together. Note that HTML will not be escaped twice even if this tag is used in a format that allows no HTML in the first place.'),
+    ];
+
+    $form['edit']['php'] = [
+      '#type' => 'checkbox',
+      '#title' => t('Evaluate as PHP code.'),
+      '#return_value' => TRUE,
+      '#description' => t('This option requires the PHP module to be enabled, and the appropriate permission.'),
+      '#default_value' => $use_php,
+    ];
+
+    foreach ($form['edit']['options']['#options'] as $key => $value) {
+      if (!empty($tag->options[$key])) {
+        $form['edit']['options']['#default_value'][] = $key;
+      }
+    }
+
+    $form['edit']['markup'] = [
+      '#type' => 'textarea',
+      '#attributes' => ['style' => 'font-family:monospace'],
+      '#title' => t('Rendering code'),
+      '#default_value' => $tag->markup,
+      '#required' => $editing_tag || $adding_tag,
+      '#description' => t('The text that [tag]content[/tag] should be replaced with, or PHP code that prints/returns the text.', ['@url' => url('admin/help/xbbcode')]),
+    ];
+
+    if (!$access_php) {
+      $form['edit']['php']['#disabled'] = TRUE;
+      $form['edit']['php']['#value'] = $form['edit']['php']['#default_value'];
+      // Imitate the behavior of filter.module on forbidden formats.
+      if ($use_php) {
+        $form['edit']['markup']['#disabled'] = TRUE;
+        $form['edit']['markup']['#resizable'] = FALSE;
+        $form['edit']['markup']['#value'] = $form['edit']['markup']['#default_value'];
+        $form['edit']['markup']['#pre_render'] = ['filter_form_access_denied'];
+      }
+    }
+
+    $form['edit']['help'] = [
+      '#type' => 'markup',
+      '#title' => t('Coding help'),
+      '#markup' => t('<p>The above field should be filled either with HTML or PHP code depending on whether you enabled the PHP code option. PHP code must be placed in &lt;?php ?&gt; enclosures, or it will be
+      printed literally.</p>
+      <p>If your tag uses static HTML, then the tag\'s content and attributes will be inserted into your code by replacing placeholders. In PHP code, they will be available in the <code>$tag</code> object.</p>
+      <dl>
+        <dt><code>{content}</code> or <code>$tag->content</code></dt>
+        <dd>The text between opening and closing tags, if the tag is not self-closing. Example: <code>[url=http://www.drupal.org]<strong>Drupal</strong>[/url]</code></dd>
+        <dt><code>{option}</code> or <code>$tag->option</code></dt>
+        <dd>The single tag attribute, if one is entered. Example: <code>[url=<strong>http://www.drupal.org</strong>]Drupal[/url]</code>.</dd>
+        <dt>any other <code>{attribute}</code> or <code>$tag->attr(\'attribute\')</code></dt>
+        <dd>The tag attribute of the same name, if it is entered. E.g: <strong>{by}</strong> or <strong><code>$tag->attr(\'by\')</code></strong> for <code>[quote&nbsp;by=<strong>Author</strong>&nbsp;date=2008]Text[/quote]</code>. If the attribute is not entered, the placeholder will be replaced with an empty string, and the <code>attr()</code> return value will be <code>NULL</code>.</dd>
+      </dl>'),
+    ];
+
+    $form['edit']['submit'] = [
+      '#type' => 'submit',
+      '#value' => t('Save'),
+      '#submit' => ['::_submitFormSave'],
+    ];
+
+    if (!empty($name) || count($tags)) {
+      $delete = [
+        '#type' => 'submit',
+        '#value' => t('Delete'),
+        '#submit' => ['::_submitFormDelete'],
+      ];
+      if (!empty($name)) {
+        $form['edit']['delete'] = $delete;
+      }
+      else {
+        $form['delete'] = $delete;
+      }
+    }
+
+    return $form;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+    if (!preg_match('/^[a-z0-9]*$/i', $form_state['values']['name'])) {
+      form_set_error('name', t('The name must be alphanumeric.'));
+    }
+
+    if ($form['edit']['name']['#default_value'] != $form_state['values']['name']) {
+      if (xbbcode_custom_tag_exists($form_state['values']['name'])) {
+        form_set_error('name', t('This name is already taken. Please delete or edit the old tag, or choose a different name.'));
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {}
+
+  /**
+   * Delete selected custom tags.
+   */
+  public function _submitFormDelete(array &$form, FormStateInterface $form_state) {
+    $delete = [];
+
+    if (!empty($form_state['values']['name'])) {
+      $delete[] = $form_state['values']['name'];
+    }
+    elseif (is_array($form_state['values']['existing'])) {
+      foreach ($form_state['values']['existing'] as $tag => $checked) {
+        if ($checked) {
+          $delete[] = $tag;
+        }
+      }
+    }
+
+    xbbcode_custom_tag_delete($delete);
+
+    $tags = '[' . implode('], [', $delete) . ']';
+
+    drupal_set_message(format_plural(count($delete), 'The tag %tags has been deleted.', 'The tags %tags have been deleted.', ['%tags' => $tags]), 'status');
+    drupal_static_reset('xbbcode_custom_tag_load');
+    xbbcode_rebuild_handlers();
+    xbbcode_rebuild_tags();
+  }
+
+  /**
+   * Save (create or update) a custom tag.
+   */
+  public function _submitFormSave(array &$form, FormStateInterface $form_state) {
+    $tag = (object) $form_state['values'];
+    $tag->name = strtolower($tag->name);
+    foreach ($tag->options as $name => $value) {
+      $tag->options[$name] = $value ? 1 : 0;
+    }
+    $tag->options['php'] = $tag->php;
+
+    if (xbbcode_custom_tag_save($tag)) {
+      if ($form['edit']['name']['#default_value']) {
+        drupal_set_message(t('Tag [@name] has been changed.', ['@name' => $tag->name]));
+      }
+      else {
+        drupal_set_message(t('Tag [@name] has been created.', ['@name' => $tag->name]));
+      }
+    }
+    $form_state['redirect'] = ['admin/config/content/xbbcode/tags', []];
+    drupal_static_reset('xbbcode_custom_tag_load');
+    xbbcode_rebuild_handlers();
+    xbbcode_rebuild_tags();
+  }
+}
