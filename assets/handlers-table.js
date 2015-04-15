e2a930807a3153fdd72ced052a17fcc976e0f017
Christoph Burschka <christoph@burschka.de>
2015-04-15 23:46:08 +0200

    Adjust the jquery selectors in accordance with the previous patch.
    Descriptions are no longer paragraphs.

--- assets/handlers-table.js
+++ assets/handlers-table.js
@@ -9,8 +9,8 @@
       $('#xbbcode-handlers select.xbbcode-tag-handler').change(function() {
         var tag = this.name.match(/tags\[(.*?)\]\[module\]/)[1];
         var module = this.value;
-        $('p.tag-' + tag).addClass('xbbcode-description-invisible');
-        $('p.tag-' + tag + '.module-' + module).removeClass('xbbcode-description-invisible');
+        $('.tag-' + tag).addClass('xbbcode-description-invisible');
+        $('.tag-' + tag + '.module-' + module).removeClass('xbbcode-description-invisible');
       });
     }
   };
