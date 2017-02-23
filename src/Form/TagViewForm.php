8566bc2c06f22e468f80dbeac74c0e0447f20ffb
Christoph Burschka <christoph@burschka.de>
2017-02-23 16:22:13 +0100

    Load twig via dependency injection

--- src/Form/TagViewForm.php
+++ src/Form/TagViewForm.php
@@ -2,17 +2,46 @@
 
 namespace Drupal\xbbcode\Form;
 
-use Drupal;
+use Drupal\Core\Entity\EntityStorageInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Render\Element;
+use Drupal\Core\Template\TwigEnvironment;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * A form for viewing a read-only BBCode tag.
  */
 class TagViewForm extends TagEditForm {
+  /**
+   * @var \Drupal\Core\Template\TwigEnvironment
+   */
+  protected $twig;
+
+  /**
+   * TagViewForm constructor.
+   *
+   * @param \Drupal\Core\Entity\EntityStorageInterface $storage
+   * @param \Drupal\Core\Template\TwigEnvironment      $twig
+   */
+  public function __construct(EntityStorageInterface $storage, TwigEnvironment $twig) {
+    parent::__construct($storage);
+    $this->twig = $twig;
+  }
 
   /**
    * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('entity_type.manager')->getStorage('xbbcode_tag'),
+      $container->get('twig')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   *
+   * @throws \Twig_Error_Loader
    */
   public function form(array $form, FormStateInterface $form_state) {
     $form = parent::form($form, $form_state);
@@ -21,7 +50,7 @@ class TagViewForm extends TagEditForm {
     if (!$form['template_code']['#default_value'] && $file = $this->entity->getTemplateFile()) {
       // The source must be loaded directly, because the template class won't
       // have it unless it is loaded from the file cache.
-      $source = Drupal::service('twig')->getLoader()->getSource($file);
+      $source = $this->twig->getLoader()->getSource($file);
       $form['template_code']['#default_value'] = $source;
       $form['template_code']['#rows'] = max(5, substr_count($source, "\n"));
     }
