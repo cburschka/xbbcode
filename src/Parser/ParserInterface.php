8246cc9127b931107ae787e0646deef7ea9e395f
Christoph Burschka <christoph@burschka.de>
2017-04-14 00:25:10 +0200

    ::parse returns a NodeElement.

--- src/Parser/ParserInterface.php
+++ src/Parser/ParserInterface.php
@@ -15,7 +15,7 @@ interface ParserInterface {
    * @param bool $prepared
    *   Whether the text has already been parsed and escaped before.
    *
-   * @return \Drupal\xbbcode\Parser\ElementInterface
+   * @return \Drupal\xbbcode\Parser\NodeElementInterface
    *   The element representing the root of the tree.
    */
   public function parse($text, $prepared = FALSE);
