0caebafd4a4c3cfb048404cfeea77bcf5fe0ce7d
Christoph Burschka <christoph@burschka.de>
2020-05-15 17:38:29 +0200

    Rework HTML filtering to be more consistent.
    - Move the reverse filtering from PreparedTagElement to a single method
      in XBBCodeFilter (refactor).
    - Do not reverse HTML filtering applied to the source, only attributes
      and option.
    
    - Apply XSS filtering to tag source as well as text elements.
    - Apply XSS filtering to the argument string part of getOuterSource().
    
    - Make PreparedTagElement mark source and outer source as safe via
      Markup::create().
    
    No safe strings have become unsafe, so existing plugins and
    configurations will experience no safety issues. Some plugins may now
    redundantly escape safe markup.

--- src/PreparedTagElement.php
+++ src/PreparedTagElement.php
@@ -2,23 +2,25 @@
 
 namespace Drupal\xbbcode;
 
-use Drupal\Component\Render\MarkupInterface;
-use Drupal\Component\Utility\Html;
+use Drupal\Component\Utility\Xss;
 use Drupal\Core\Render\Markup;
 use Drupal\xbbcode\Parser\Processor\TagProcessorInterface;
 use Drupal\xbbcode\Parser\Tree\ElementInterface;
 use Drupal\xbbcode\Parser\Tree\NodeElementInterface;
 use Drupal\xbbcode\Parser\Tree\OutputElementInterface;
 use Drupal\xbbcode\Parser\Tree\TagElementInterface;
-use Drupal\xbbcode\Parser\Tree\TextElement;
 
 /**
  * Adapter for the tag element that marks markup as safe.
  *
- * The source and arguments are examined for unencoded HTML.
- * If there is none, they are marked as safe to avoid double-escaping entities.
+ * This is used to allow Twig in template tags to automatically filter unsafe
+ * strings while leaving safe markup intact.
  *
- * The rendered content is always marked as safe.
+ * The content, source and outer source of the tag are marked as safe.
+ * In the outer source, the argument string is run through XSS filtering to
+ * ensure it is indeed safe.
+ *
+ * The option and attribute values are not safe, and therefore left as is.
  */
 class PreparedTagElement implements TagElementInterface {
   /**
@@ -29,32 +31,11 @@ class PreparedTagElement implements TagElementInterface {
   protected $tag;
 
   /**
-   * The full tag argument string.
+   * The outer source.
    *
    * @var \Drupal\Component\Render\MarkupInterface
    */
-  protected $argument;
-
-  /**
-   * The tag attributes.
-   *
-   * @var \Drupal\Component\Render\MarkupInterface[]
-   */
-  protected $attributes = [];
-
-  /**
-   * The tag option.
-   *
-   * @var \Drupal\Component\Render\MarkupInterface
-   */
-  protected $option;
-
-  /**
-   * The tag input.
-   *
-   * @var \Drupal\Component\Render\MarkupInterface
-   */
-  protected $source;
+  protected $outerSource;
 
   /**
    * PreparedTagElement constructor.
@@ -64,23 +45,6 @@ class PreparedTagElement implements TagElementInterface {
    */
   public function __construct(TagElementInterface $tag) {
     $this->tag = $tag;
-
-    // If the argument string is free of raw HTML, decode its entities.
-    if (!preg_match('/[<>"\']/', $tag->getArgument())) {
-      $this->argument = Html::decodeEntities($tag->getArgument());
-      $this->attributes = array_map([Html::class, 'decodeEntities'], $tag->getAttributes());
-      $this->option = Html::decodeEntities($tag->getOption());
-    }
-    if (!preg_match('/[<>"\']/', $tag->getSource())) {
-      $this->source = Html::decodeEntities($tag->getSource());
-    }
-
-    // Wrap text elements in markup interface; the input is already filtered.
-    foreach ($this->getChildren() as $child) {
-      if ($child instanceof TextElement) {
-        $child->setText(Markup::create($child->getText()));
-      }
-    }
   }
 
   /**
@@ -94,50 +58,85 @@ class PreparedTagElement implements TagElementInterface {
    * {@inheritdoc}
    */
   public function getArgument(): string {
-    return $this->argument ?: $this->tag->getArgument();
+    return $this->tag->getArgument();
   }
 
   /**
    * {@inheritdoc}
    */
   public function getAttribute(string $name): ?string {
-    $attributes = $this->getAttributes();
-    return $attributes[$name] ?? NULL;
+    return $this->tag->getAttribute($name);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setAttribute(string $name, string $value = NULL): void {
+    $this->tag->setAttribute($name, $value);
   }
 
   /**
    * {@inheritdoc}
    */
   public function getAttributes(): array {
-    return $this->attributes ?: $this->tag->getAttributes();
+    return $this->tag->getAttributes();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setAttributes(array $attributes): void {
+    $this->tag->setAttributes($attributes);
   }
 
   /**
    * {@inheritdoc}
    */
   public function getOption(): string {
-    return $this->option ?: $this->tag->getOption();
+    return $this->tag->getOption();
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getContent(): MarkupInterface {
+  public function setOption(string $value): void {
+    $this->tag->setOption($value);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getContent() {
     return Markup::create($this->tag->getContent());
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getSource(): string {
-    return $this->source ?: $this->tag->getSource();
+  public function getSource() {
+    return Markup::create($this->tag->getSource());
   }
 
   /**
    * {@inheritdoc}
    */
-  public function getOuterSource(): string {
-    return Markup::create($this->tag->getOuterSource());
+  public function setSource(string $source): void {
+    $this->tag->setSource($source);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getOuterSource() {
+    // Reconstruct the opening and closing tags, but render the content.
+    if (!isset($this->outerSource)) {
+      $name = $this->tag->getName();
+      // The argument string must be made safe before rendering.
+      $argument = Xss::filterAdmin($this->tag->getArgument());
+      $content = $this->tag->getContent();
+      $this->outerSource = Markup::create("[{$name}{$argument}]{$content}[/{$name}]");
+    }
+    return $this->outerSource;
   }
 
   /**
